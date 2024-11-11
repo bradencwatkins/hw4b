3c90d5dea457b1dc920b47eb6a73466e8ef5decc
bradencwatkins <bradencwatkins@gmail.com>
2024-11-11 11:23:45 -0700

    made all of the class functions

--- DLList.h
+++ DLList.h
@@ -10,13 +10,83 @@ using namespace std;
 template<class T>
 class DLList {
 public:
+    struct Node {
+        Node* prev;
+        Node next;
+        T value;
+
+        Node(T val) : next(nullptr), prev(nullptr), value(val) {}
+    };
+
+    DLList() {
+        head = nullptr;
+        tail = nullptr;
+        numItems = 0;
+    }
+
+    ~DLList() {
+        clear();
+    }
+
+    const Node* get_head() const {
+        return head;
+    }
+
+    void push_front(T item) {
+        
+    }
+
+    void push_back(T item) {
+
+    }
+
+    void insert(T item, int position) {
+
+    }
+
+    void pop_front() {
+
+    }
+
+    void pop_back() {
+
+    }
+
+    void remove(int position) {
+
+    }
+
+    const T& front() const {
+
+    }
+
+    const T& back() const {
+
+    }
+
+    const T& at(int index) const {
+
+    }
+
+    bool contains(const T& item) {
+
+    }
+
+    int size() const {
+        return numItems;
+    }
+
+    void clear() {
+
+    }
 
 
 
-
-    
 
 
 private:
+    Node* head;
+    Node* tail;
+    int numItems;
 };
 
